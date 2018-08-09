ab81782a5838718c61e41718b8c3e18dc88016ef
fadzbobby <fadzbobby95@gmail.com>
2018-08-09 21:37:53 +0800

    feat: create rename function

---
+++ MovieRename.py
@@ -0,0 +1,34 @@
+import sys, os
+from Rename import Rename
+
+if len(sys.argv)>1:
+	file_path = sys.argv[1]
+	pass
+else:
+	file_path = "C:\\dasdqwe\\dasd\\Deadpool.2.2018.Super.Duper.Cut.UNRATED.1080p. WEBRip.akoam.net.mkv"
+	pass
+
+print("file_path:",file_path)
+
+file_name_ori = os.path.basename(file_path)
+file_name = Rename.rename(file_name_ori)
+
+file_folder = os.path.dirname(file_path)
+file_new_folder = Rename.rename(file_name_ori)
+
+if os.path.isfile(file_path):
+	new_path = os.path.join(file_folder,file_new_folder+"\\"+file_name+Rename.getExtension(file_name_ori))
+	if not os.path.exists(file_new_folder):
+	    os.makedirs(file_new_folder)
+else:
+	new_path = os.path.join(file_folder,file_name+Rename.getExtension(file_name_ori))
+	pass
+
+os.rename(file_path, new_path);
+
+print("new_path:",new_path)
+
+print("file_folder:",file_folder)
+print("file_name:",file_name)
+print("Rename:",Rename.getExtension(file_name_ori))
+
---
+++ Rename/Rename.py
@@ -0,0 +1,106 @@
+import os, re
+ripQuality = ["4320p", "2160p", "1440p", "1080p", "1080i", "720p", "720i", "480p", "480i", "360p", "240p"]
+ripQualityWidth = ["7680", "3840", "2560", "1920", "1920", "1280", "1280", "858", "858", "480", "352"]
+pirateRelease=['CAMRip','CAM',
+'TS','HDTS','TELESYNC','PDVD',
+'WP','WORKPRINT',
+'TC','TELECINE',
+'PPV','PPVRip',
+'SCR','SCREENER','DVDSCR','DVDSCREENER','BDSCR',
+'DDC',
+'R5','R5.LINE','R5.AC3.5.1.HQ',
+'DVDRip',
+'DVDR','DVD-Full','Full-Rip','ISO rip','DVD-5','DVD-9',
+'DSR','DSRip','SATRip','DTHRip','DVBRip','HDTV','PDTV','TVRip','HDTVRip',
+'VODRip','VODR',
+'WEBDL','WEB-DL','WEB','HDRip',
+'WEB-Rip','WEBRip','HDRip',
+'WEB-Cap','WEBCAP',
+'BDRip','BRRip','Blu-Ray','BluRay','BDR','BD5','BD9','BD25','BD50'];
+
+def getAttribute(OriginalName='',arrfile='',mode=1):
+	movieYearIndex=0;
+	movieYear='';
+
+	movieQualityIndex=0;
+	movieQuality='';
+
+	moviePirateReleaseIndex=0
+	moviePirateRelease=''
+
+	movieTitle=''
+	movieTitleEnd=''
+	if ']' in OriginalName and '[' in OriginalName:
+		inSquare = OriginalName.split("]")[0].split("[")[1].strip()
+
+		# replace delete bracket
+		OriginalName = re.sub(r'\[[^)]*\]', '', OriginalName).strip()
+		if inSquare !="":
+			OriginalName+=" "+inSquare
+			pass
+
+	if '(' in OriginalName: OriginalName = re.sub(r'\(', '', OriginalName)
+	if ')' in OriginalName: OriginalName = re.sub(r'\)', '', OriginalName)
+	if "_" in OriginalName: OriginalName = OriginalName.replace("_"," ")
+
+	if mode==0: fileAttr = OriginalName.split(' ')
+
+	else:fileAttr = OriginalName.replace('.',' ').split(' ')
+
+	for x in range(0,len(fileAttr)):
+		i =len(fileAttr)-1-x
+		# check for 4 digit and it will be year
+		if len(fileAttr[i]) == 4 and fileAttr[i].isdigit():
+			movieYear=fileAttr[i];
+			movieYearIndex=i
+			break
+
+	for x in range(movieYearIndex+1,len(fileAttr)):
+		if fileAttr[x].lower() in ripQuality:
+			movieQuality=fileAttr[x].lower()
+			movieQualityIndex=x;
+		if fileAttr[x].lower() in [item.lower() for item in pirateRelease]:
+			moviePirateRelease=pirateRelease[[item.lower() for item in pirateRelease].index(fileAttr[x].lower())]
+			moviePirateReleaseIndex=x;
+
+	arrAtrx =[s for s in [movieYearIndex,movieQualityIndex,moviePirateReleaseIndex] if s!=0];
+	for x in range(0,len(fileAttr)):
+		if not arrAtrx:
+			movieTitle = OriginalName;
+			break
+		else:
+			if x not in arrAtrx and x < min(arrAtrx):
+				movieTitle +=' '+fileAttr[x];
+			if x not in arrAtrx and x > max(arrAtrx):
+				movieTitleEnd +=' '+fileAttr[x];
+	ftg=[]
+	if os.path.isfile('./'+arrfile):
+		for ghj in os.listdir("./"+arrfile):
+			if ghj.lower().endswith(".srt") or ghj.lower().endswith(".ass"):
+				ftg.append(ghj);
+				pass
+			pass
+		pass
+	checksbttle = len(ftg)>0
+	checkposter = os.path.isfile('./'+arrfile+'/poster.jpg') 
+	return [movieTitle.strip(),movieYear,moviePirateRelease,movieQuality,movieTitleEnd.strip(),('Y' if checksbttle else 'N'),('Y' if checkposter else 'N'),('2' if '' in [movieYear,moviePirateRelease,movieQuality] else '3'),arrfile]
+def rename(path, format="{} ({}) {} {} {}"):
+	extension=""
+	if path.endswith(".mkv") or path.endswith(".mp4") or path.endswith(".avi") or path.endswith(".flv"):
+		path = path.rsplit('.',1)[0]
+		pass
+
+	property = getAttribute(path,mode=1);
+	new_name = format.format(property[0],property[1],property[2],property[3],property[4]).strip()
+	return re.sub(' +',' ',new_name)
+	pass
+def getExtension(path):
+	extension = ""
+	try:
+		if path.endswith(".mkv") or path.endswith(".mp4") or path.endswith(".avi") or path.endswith(".flv") or path.endswith(".rmvb"):
+			extension = "."+path.rsplit('.',1)[1]
+		pass
+	except Exception as e:
+		pass
+	return extension;
+	pass
